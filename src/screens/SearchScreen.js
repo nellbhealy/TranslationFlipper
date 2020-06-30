9649277f578f90c1f56fa24ede2e07a24b93a9ee
Liam Becker <liambecker28@gmail.com>
2020-06-30 13:14:11 -0400

    Set up stack navigation

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const SearchScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text>Some text!</Text>
+      <Text>This is where the user will search for words!</Text>
+      <Text>I dig the grey background</Text>
+    </View>
+  );
+};
+
+export default SearchScreen;
