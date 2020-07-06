5b0515384c31101034f15ec0098b96159f8a35e5
Liam Becker <liambecker28@gmail.com>
2020-07-06 15:42:04 -0400

    Remove starter home page, add learning screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
 import SearchScreen from './src/screens/SearchScreen/SearchScreen';
+import LearningScreen from './src/screens/LearningScreen/LearningScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="Learn" component={LearningScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/LearningScreen/LearningScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const LearningScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text>Learning Screen</Text>
+    </View>
+  );
+};
+
+export default LearningScreen;
