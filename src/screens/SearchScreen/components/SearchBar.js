ffe6f0e2bf1a7deb845357d419f50a705566c800
Liam Becker <liambecker28@gmail.com>
2020-07-06 09:09:53 -0400

    Add minimal search bar styling

--- src/screens/SearchScreen/components/SearchBar.js
+++ src/screens/SearchScreen/components/SearchBar.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import {View, TextInput, Button} from 'react-native';
+import {View, TextInput, Button, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 
 const PLACEHOLDER_TEXT = 'What word do you want to look up?';
+const MARGIN_HORIZ = 16;
+
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    flex: 3,
+  },
+  button: {
+    flex: 1,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: MARGIN_HORIZ,
+    backgroundColor: 'white',
+  },
+});
 
 const SearchBar = props => {
   return (
-    <View>
+    <View style={styles.fixToText}>
       <TextInput
         placeholder={PLACEHOLDER_TEXT}
         onChangeText={inputText => props.setSearchTerm(inputText)}
         onEndEditing={props.updateData}
+        style={styles.input}
       />
-      <Button onPress={props.updateData} title="search" />
+      <View style={styles.button}>
+        <Button onPress={props.updateData} title="search" />
+      </View>
     </View>
   );
 };
